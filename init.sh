d6819b9895cfbd04903804e1812e9d2ebea24ac0
chiefmikey <wolfemikl@gmail.com>
2021-08-18 21:35:09 -0600

    Remove unimportant files on startup

--- init.sh
+++ init.sh
@@ -4,5 +4,23 @@ if [ "$(cat /home/ec2-user/mikl/stop.txt)" = stop ]; then
   docker-compose -f /home/ec2-user/mikl/docker-compose.yaml down --remove-orphans
 fi
 
+sudo yum update -y
+
+mv /home/ec2-user/mikl/docker-compose.yaml /home/ec2-user
+mv /home/ec2-user/mikl/LICENSE /home/ec2-user
+mv /home/ec2-user/mikl/README.md /home/ec2-user
+mv /home/ec2-user/mikl/init.sh /home/ec2-user
+mv /home/ec2-user/mikl/stop.sh /home/ec2-user
+mv /home/ec2-user/mikl/stop.txt /home/ec2-user
+mv /home/ec2-user/mikl/appspec.yaml /home/ec2-user
+rm -R /home/ec2-user/mikl/*
+mv /home/ec2-user/docker-compose.yaml /home/ec2-user/mikl
+mv /home/ec2-user/LICENSE /home/ec2-user/mikl
+mv /home/ec2-user/README.md /home/ec2-user/mikl
+mv /home/ec2-user/init.sh /home/ec2-user/mikl
+mv /home/ec2-user/stop.sh /home/ec2-user/mikl
+mv /home/ec2-user/stop.txt /home/ec2-user/mikl
+mv /home/ec2-user/appspec.yaml /home/ec2-user/mikl
+
 docker-compose -f /home/ec2-user/mikl/docker-compose.yaml pull && \
 docker-compose -f /home/ec2-user/mikl/docker-compose.yaml up -d
