04de5379fad00ea0c2d29f98c4ac2ee64e716b1d
chiefmikey <wolfemikl@gmail.com>
2021-08-18 18:44:46 -0600

    Include stop condition

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+if [ "$(cat /home/ec2-user/mikl/stop.txt)" = stop ]; then
+  docker-compose -f /home/ec2-user/mikl/docker-compose.yaml down --remove-orphans
+fi
+
 docker-compose -f /home/ec2-user/mikl/docker-compose.yaml up -d
