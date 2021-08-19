95782a3f03d29941c4272b1106f1d5a98d4bead1
chiefmikey <wolfemikl@gmail.com>
2021-08-18 19:29:31 -0600

    Fix docker pull

--- init.sh
+++ init.sh
@@ -4,4 +4,5 @@ if [ "$(cat /home/ec2-user/mikl/stop.txt)" = stop ]; then
   docker-compose -f /home/ec2-user/mikl/docker-compose.yaml down --remove-orphans
 fi
 
-docker-compose pull && docker-compose -f /home/ec2-user/mikl/docker-compose.yaml up -d
+docker-compose -f /home/ec2-user/mikl/docker-compose.yaml pull && \
+docker-compose -f /home/ec2-user/mikl/docker-compose.yaml up -d
