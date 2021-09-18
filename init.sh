3f304f866979aee1865d9c6e7296e68e8ffc0569
chiefmikey <wolfemikl@gmail.com>
2021-09-18 15:30:50 -0600

    Update init file rm

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ fi
 
 sudo yum update -y
 
-sudo rm -rf /home/ec2-user/mikl/.github /home/ec2-user/mikl/.dockerignore /home/ec2-user/mikl/.dropboxignore /home/ec2-user/mikl/.eslintignore /home/ec2-user/mikl/.eslintrc.cjs /home/ec2-user/mikl/.gitignore /home/ec2-user/mikl/.prettierignore /home/ec2-user/mikl/.prettierrc /home/ec2-user/mikl/.stylelintignore /home/ec2-user/mikl/.stylelintrc.json /home/ec2-user/mikl/package-lock.json /home/ec2-user/mikl/package.json
+sudo rm -rf /home/ec2-user/mikl/.github /home/ec2-user/mikl/.dockerignore /home/ec2-user/mikl/package.json
 
 docker-compose -f /home/ec2-user/mikl/docker-compose.yaml pull && \
 docker-compose -f /home/ec2-user/mikl/docker-compose.yaml up -d
