116f57b00a8fdf0da69946cec49940a765331fb7
chiefmikey <wolfemikl@gmail.com>
2021-10-01 23:32:06 -0600

    Remove symlink

--- init.sh
+++ init.sh
@@ -12,7 +12,6 @@ touch $INSTANCE_ALREADY_STARTED
   sudo yum install -y git
   sudo curl -L "https://github.com/docker/compose/releases/download/2.0.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
-  sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
   echo $(docker-compose version)
   sudo reboot
 else
