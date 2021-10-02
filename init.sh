076de2c9ddbd4c3571846e41ee0f7300747b9629
chiefmikey <wolfemikl@gmail.com>
2021-10-02 14:14:53 -0600

    Update paths to absolute

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 INSTANCE_ALREADY_STARTED="INSTANCE_ALREADY_STARTED_PLACEHOLDER"
-if [ ! -e $INSTANCE_ALREADY_STARTED ]; then
-touch $INSTANCE_ALREADY_STARTED
+if [ ! -e /home/ec2-user/$INSTANCE_ALREADY_STARTED ]; then
+touch /home/ec2-user/$INSTANCE_ALREADY_STARTED
   echo "-- First instance startup --"
   sudo yum update -y
   sudo amazon-linux-extras install docker
@@ -10,9 +10,9 @@ touch $INSTANCE_ALREADY_STARTED
   sudo usermod -a -G docker ec2-user
   sudo chkconfig docker on
   sudo yum install -y git
-  sudo mkdir -p ~/.docker/cli-plugins/
-  sudo curl -SL https://github.com/docker/compose/releases/download/v2.0.0/docker-compose-linux-amd64 -o ~/.docker/cli-plugins/docker-compose && \
-  sudo chmod +x ~/.docker/cli-plugins/docker-compose && \
+  sudo mkdir -p /home/ec2-user/.docker/cli-plugins/
+  sudo curl -SL https://github.com/docker/compose/releases/download/v2.0.0/docker-compose-linux-amd64 -o /home/ec2-user/.docker/cli-plugins/docker-compose && \
+  sudo chmod +x /home/ec2-user/.docker/cli-plugins/docker-compose && \
   # sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
   docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml pull && \
   docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml up -d
