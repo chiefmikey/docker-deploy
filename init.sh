cffd37682390c66738dadf1b3295d2914d6670e4
chiefmikey <wolfemikl@gmail.com>
2021-10-04 16:01:53 -0600

    Update init

--- init.sh
+++ init.sh
@@ -5,17 +5,19 @@ if [ ! -e /home/ec2-user/$INSTANCE_ALREADY_STARTED ]; then
 touch /home/ec2-user/$INSTANCE_ALREADY_STARTED
   echo "-- First instance startup --"
   sudo yum update -y
-  sudo amazon-linux-extras install docker
-  sudo service docker start
-  sudo usermod -a -G docker ec2-user
-  sudo chkconfig docker on
-  echo $(docker info)
   sudo yum install -y git
   sudo yum install -y wget
   sudo mkdir -p /home/ec2-user/.docker/cli-plugins/
   sudo wget -O /home/ec2-user/.docker/cli-plugins/docker-compose https://github.com/docker/compose/releases/download/v2.0.0/docker-compose-linux-amd64
   sudo chmod +x /home/ec2-user/.docker/cli-plugins/docker-compose
   # sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+  sudo amazon-linux-extras install docker
+  sudo service docker start
+  sudo usermod -a -G docker ec2-user
+  sudo systemctl start docker
+  sudo systemctl status docker
+  sudo systemctl enable docker
+  sudo chkconfig docker on
   docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml pull
   docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml up -d
 else
