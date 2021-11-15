7219ab1c3d31c26350235612eb3bf80b4560f182
chiefmikey <wolfemikl@gmail.com>
2021-11-15 15:11:39 -0700

    Link docker compose

--- init.sh
+++ init.sh
@@ -10,7 +10,7 @@ touch /home/ec2-user/$INSTANCE_ALREADY_STARTED
   sudo mkdir -p /home/ec2-user/.docker/cli-plugins/
   sudo wget -O /home/ec2-user/.docker/cli-plugins/docker-compose https://github.com/docker/compose/releases/download/v2.1.1/docker-compose-linux-x86_64
   sudo chmod +x /home/ec2-user/.docker/cli-plugins/docker-compose
-  # sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+  sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
   sudo amazon-linux-extras install docker
   sudo usermod -a -G docker ec2-user
   sudo systemctl start docker
@@ -18,14 +18,14 @@ touch /home/ec2-user/$INSTANCE_ALREADY_STARTED
   sudo systemctl enable docker
   sudo chkconfig docker on
   docker version
-  /home/ec2-user/.docker/cli-plugins/docker-compose -f /home/ec2-user/mikl.io/docker-compose.yaml pull
-  /home/ec2-user/.docker/cli-plugins/docker-compose -f /home/ec2-user/mikl.io/docker-compose.yaml up -d
+  docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml pull
+  docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml up -d
 else
   echo "-- Not first instance startup --"
   sudo yum update -y
   if [ "$(cat /home/ec2-user/mikl.io/stop.txt)" = stop ]; then
-    /home/ec2-user/.docker/cli-plugins/docker-compose -f /home/ec2-user/mikl.io/docker-compose.yaml down --remove-orphans
+    docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml down --remove-orphans
   fi
-  /home/ec2-user/.docker/cli-plugins/docker-compose -f /home/ec2-user/mikl.io/docker-compose.yaml pull
-  /home/ec2-user/.docker/cli-plugins/docker-compose -f /home/ec2-user/mikl.io/docker-compose.yaml up -d
+  docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml pull
+  docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml up -d
 fi
