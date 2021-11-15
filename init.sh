a0890cadca4ab92cc79e1f9b36ba37256320a76a
chiefmikey <wolfemikl@gmail.com>
2021-11-15 14:48:27 -0700

    Update docker compose executable to absolute path

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ touch /home/ec2-user/$INSTANCE_ALREADY_STARTED
   sudo yum install -y git
   sudo yum install -y wget
   sudo mkdir -p /home/ec2-user/.docker/cli-plugins/
-  sudo wget -O /home/ec2-user/.docker/cli-plugins/docker-compose https://github.com/docker/compose/releases/download/v2.1.1/docker-compose-linux-amd64
+  sudo wget -O /home/ec2-user/.docker/cli-plugins/docker-compose https://github.com/docker/compose/releases/download/v2.1.1/docker-compose-linux-x86_64
   sudo chmod +x /home/ec2-user/.docker/cli-plugins/docker-compose
   # sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
   sudo amazon-linux-extras install docker
@@ -18,14 +18,14 @@ touch /home/ec2-user/$INSTANCE_ALREADY_STARTED
   sudo systemctl enable docker
   sudo chkconfig docker on
   docker version
-  docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml pull
-  docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml up -d
+  /home/ec2-user/.docker/cli-plugins/docker-compose -f /home/ec2-user/mikl.io/docker-compose.yaml pull
+  /home/ec2-user/.docker/cli-plugins/docker-compose -f /home/ec2-user/mikl.io/docker-compose.yaml up -d
 else
   echo "-- Not first instance startup --"
   sudo yum update -y
   if [ "$(cat /home/ec2-user/mikl.io/stop.txt)" = stop ]; then
-    docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml down --remove-orphans
+    /home/ec2-user/.docker/cli-plugins/docker-compose -f /home/ec2-user/mikl.io/docker-compose.yaml down --remove-orphans
   fi
-  docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml pull
-  docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml up -d
+  /home/ec2-user/.docker/cli-plugins/docker-compose -f /home/ec2-user/mikl.io/docker-compose.yaml pull
+  /home/ec2-user/.docker/cli-plugins/docker-compose -f /home/ec2-user/mikl.io/docker-compose.yaml up -d
 fi
