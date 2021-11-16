af44b1f958d82f83d78a5b421b017f91b21eb1c5
chiefmikey <wolfemikl@gmail.com>
2021-11-15 23:30:24 -0700

    Update init script

--- init.sh
+++ init.sh
@@ -7,13 +7,13 @@ touch /home/ec2-user/$INSTANCE_ALREADY_STARTED
   sudo yum update -y
   sudo yum install -y git
   sudo yum install -y wget
-  sudo mkdir -p /home/ec2-user/.docker/cli-plugins/
-  sudo wget -O /home/ec2-user/.docker/cli-plugins/docker-compose https://github.com/docker/compose/releases/download/v2.1.1/docker-compose-linux-x86_64
+  mkdir -p /home/ec2-user/.docker/cli-plugins/
+  wget -O /home/ec2-user/.docker/cli-plugins/docker-compose https://github.com/docker/compose/releases/download/v2.1.1/docker-compose-linux-x86_64
   sudo chmod +x /home/ec2-user/.docker/cli-plugins/docker-compose
   sudo ln -s /home/ec2-user/.docker/cli-plugins/docker-compose /usr/bin/docker-compose
   sudo amazon-linux-extras install docker
   sudo usermod -a -G docker $USER
-  sudo chmod 666 /var/run/docker.sock
+  # sudo chmod 666 /var/run/docker.sock
   sudo su $USER
   sudo systemctl enable --now docker
   sudo chkconfig docker on
