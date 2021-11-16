d9eac820206e72a41e38e2c916b73c72e1ad5b03
chiefmikey <wolfemikl@gmail.com>
2021-11-15 18:43:06 -0700

    Update with chmod

--- init.sh
+++ init.sh
@@ -14,6 +14,7 @@ touch /home/ec2-user/$INSTANCE_ALREADY_STARTED
   sudo amazon-linux-extras install docker
   sudo groupadd docker
   sudo usermod -aG docker ec2-user
+  sudo chmod 666 /var/run/docker.sock
   sudo su ec2-user
   sudo systemctl start docker
   sudo systemctl status docker
