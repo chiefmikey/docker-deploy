a89798e84317726b77229387d0dc03d35b991620
chiefmikey <wolfemikl@gmail.com>
2021-11-15 20:19:07 -0700

    Sudo everything

--- init.sh
+++ init.sh
@@ -4,26 +4,26 @@ INSTANCE_ALREADY_STARTED="INSTANCE_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e /home/ec2-user/$INSTANCE_ALREADY_STARTED ]; then
 touch /home/ec2-user/$INSTANCE_ALREADY_STARTED
   echo "-- First instance startup --"
-  yum update -y
-  yum install -y git
-  yum install -y wget
-  mkdir -p /home/ec2-user/.docker/cli-plugins/
-  wget -O /home/ec2-user/.docker/cli-plugins/docker-compose https://github.com/docker/compose/releases/download/v2.1.1/docker-compose-linux-x86_64
-  chmod +x /home/ec2-user/.docker/cli-plugins/docker-compose
-  ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-  amazon-linux-extras install docker
-  usermod -aG docker ec2-user
-  su ec2-user
-  systemctl enable --now docker
-  chkconfig docker on
-  docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml pull
-  docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml up -d
+  sudo yum update -y
+  sudo yum install -y git
+  sudo yum install -y wget
+  sudo mkdir -p /home/ec2-user/.docker/cli-plugins/
+  sudo wget -O /home/ec2-user/.docker/cli-plugins/docker-compose https://github.com/docker/compose/releases/download/v2.1.1/docker-compose-linux-x86_64
+  sudo chmod +x /home/ec2-user/.docker/cli-plugins/docker-compose
+  sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+  sudo amazon-linux-extras install docker
+  sudo usermod -aG docker ec2-user
+  sudo su ec2-user
+  sudo systemctl enable --now docker
+  sudo chkconfig docker on
+  sudo docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml pull
+  sudo docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml up -d
 else
   echo "-- Not first instance startup --"
-  yum update -y
+  sudo yum update -y
   if [ "$(cat /home/ec2-user/mikl.io/stop.txt)" = stop ]; then
-    docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml down --remove-orphans
+    sudo docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml down --remove-orphans
   fi
-  docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml pull
-  docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml up -d
+  sudo docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml pull
+  sudo docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml up -d
 fi
