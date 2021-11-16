48b5c792a4fcf9f53799edf2d6d30fe2709c140d
chiefmikey <wolfemikl@gmail.com>
2021-11-15 17:03:50 -0700

    Update with su -s

--- init.sh
+++ init.sh
@@ -13,13 +13,17 @@ touch /home/ec2-user/$INSTANCE_ALREADY_STARTED
   sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
   sudo amazon-linux-extras install docker
   sudo usermod -a -G docker ec2-user
+  sudo su -s ec2-user
   sudo systemctl start docker
+  sudo systemctl status docker
+  sudo systemctl enable docker
+  sudo chkconfig docker on
+  docker version
   docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml pull
   docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml up -d
 else
   echo "-- Not first instance startup --"
   sudo yum update -y
-  sudo systemctl start docker
   if [ "$(cat /home/ec2-user/mikl.io/stop.txt)" = stop ]; then
     docker compose -f /home/ec2-user/mikl.io/docker-compose.yaml down --remove-orphans
   fi
